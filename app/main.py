e0ee5f8a15c42968745e839d20ab25558df0ed70
nikon4uk <97480647+nikon4uk@users.noreply.github.com>
2022-01-27 12:37:18 +0200

    was fixed cpndition logic

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ class DeliveryDrone(FlyingRobot):
         self.current_load = current_load
 
     def hook_load(self, cargo: Cargo):
-        if self.current_load is None and self.max_load_weight > cargo.weight:
+        if self.current_load is None and self.max_load_weight >= cargo.weight:
             self.current_load = cargo
 
     def unhook_load(self):
