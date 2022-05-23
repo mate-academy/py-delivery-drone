5e3081983a7d080708cc9b8e76812373ce6d8bb8
Taras Nester <tarasnester414@gmail.com>
2022-05-23 15:21:19 +0300

    created robot classes

--- app/main.py
+++ app/main.py
@@ -7,10 +7,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None):
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1):
         self.coords[1] += step
@@ -32,8 +29,6 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None):
         if coords is None:
             coords = [0, 0, 0]
-        else:
-            coords = coords
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1):
