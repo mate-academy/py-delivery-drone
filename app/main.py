e46758c0d1cf25bc74356f1448d5b01184f44f3b
Artem Kazakov <artemkazakov947@gmail.com>
2022-06-14 16:14:48 +0300

    The ternary method in conctructor was implemented

--- app/main.py
+++ app/main.py
@@ -7,9 +7,7 @@ class BaseRobot:
     def __init__(self, name, weight, coords=None):
         self.name = name
         self.weight = weight
-        self.coords = coords
-        if self.coords is None:
-            self.coords = [0, 0]
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step=1):
         self.coords[1] += step
@@ -30,9 +28,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name, weight, coords=None):
         super().__init__(name, weight, coords=coords)
-        self.coords = coords
-        if self.coords is None:
-            self.coords = [0, 0, 0]
+        self.coords = [0, 0, 0] if coords is None else coords
 
     def go_up(self, step=1):
         self.coords[2] += step
