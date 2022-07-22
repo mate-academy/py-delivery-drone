c7aecd3e9d00d392f895cb8813b559c0656f6c3f
Dima Povkh <dimapovkh@gmail.com>
2022-07-22 23:09:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ class DeliveryDrone(FlyingRobot):
                  max_load_weight, coords=None, current_load=None):
         super().__init__(name, weight, coords)
         if coords is None:
-            self.coords = [0, 0, 0]  # [X, Y, Z]
+            self.coords = [0, 0, 0]
         else:
             self.coords = coords
         self.max_load_weight = max_load_weight
@@ -61,32 +61,3 @@ class DeliveryDrone(FlyingRobot):
 
     def unhook_load(self):
         self.current_load = None
-
-
-cargo = Cargo(14)
-drone = DeliveryDrone(name="Jim", weight=18, coords=[11, -4, 16],
-                      max_load_weight=20, current_load=None)
-drone.hook_load(cargo)
-print(drone.current_load)
-# is cargo
-print(drone.name)
-print(drone.coords)
-cargo2 = Cargo(2)
-drone.hook_load(cargo2)
-# drone.current_load is cargo
-
-# didn't hook cargo2, cargo already in current load
-# flying_robot = FlyingRobot(name="Mike", weight=11)
-# flying_robot.go_up(10)
-# print(flying_robot.go_forward())
-# print(flying_robot.coords) # = [0, 0, 10]
-# flying_robot.go_down(1)
-# print(flying_robot.go_left(7))
-# print(flying_robot.coords) # = [0, 0, 9]
-
-# robot = BaseRobot(name="Walle", weight=34, coords=[3, -2])
-# robot.go_forward()
-# print(robot.coords) # == [3, -1]
-# robot.go_right(5)
-# print(robot.coords) # == [8, -1]
-# # print(robot.get_info())
