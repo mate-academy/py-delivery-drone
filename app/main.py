b6e8877a8aa62dd702f50a4541aced7b4b97024e
Dima Povkh <dimapovkh@gmail.com>
2022-07-25 13:58:32 +0200

    I have used ternary operator insted of several cod`s lines

--- app/main.py
+++ app/main.py
@@ -7,10 +7,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords=None):
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords if coords is not None else [0, 0]
 
     def go_forward(self, step: int = 1):
         self.coords[1] += step
@@ -31,10 +28,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords=None):
         super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords if coords is not None else [0, 0, 0]
 
     def go_up(self, step=1):
         self.coords[2] += step
@@ -47,10 +41,8 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str, weight: int,
                  max_load_weight, coords=None, current_load=None):
         super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords if coords is not None else [0, 0, 0]
+
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
