4873d19643cb2c665fec388ac8dc8845a2dcaec7
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-08-09 18:31:31 +0300

    One line and removed return

--- app/main.py
+++ app/main.py
@@ -7,25 +7,19 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None):
         self.name = name
         self.weight = weight
-        if coords is None:
-            coords = [0, 0]
-        self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1):
         self.coords[1] += step
-        return self
 
     def go_back(self, step: int = 1):
         self.coords[1] -= step
-        return self
 
     def go_right(self, step: int = 1):
         self.coords[0] += step
-        return self
 
     def go_left(self, step: int = 1):
         self.coords[0] -= step
-        return self
 
     def get_info(self):
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -34,17 +28,13 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None):
         super().__init__(name, weight, coords)
-        if coords is None:
-            coords = [0, 0, 0]
-        self.coords = coords
+        self.coords = [0, 0, 0] if coords is None else coords
 
     def go_up(self, step: int = 1):
         self.coords[2] += step
-        return self
 
     def go_down(self, step: int = 1):
         self.coords[2] -= step
-        return self
 
 
 class DeliveryDrone(FlyingRobot):
