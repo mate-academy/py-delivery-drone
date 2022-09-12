753977ababcac41e4beede9d912b56953c373935
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-12 16:36:02 +0300

    Refactored self.coords in one line

--- app/main.py
+++ app/main.py
@@ -7,10 +7,7 @@ class BaseRobot:
     def __init__(self, name, weight, coords=None):
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def get_info(self):
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -31,10 +28,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name, weight, coords=None):
         super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0, 0] if coords is None else coords
 
     def go_up(self, step=1):
         self.coords[2] += step
@@ -46,10 +40,7 @@ class FlyingRobot(BaseRobot):
 class DeliveryDrone(FlyingRobot):
     def __init__(self, name, weight, coords=None,
                  max_load_weight=0, current_load=None):
-        if coords is None:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0, 0] if coords is None else coords
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
