1665a63a2b7ca3b8b4baa93f95720d51143c588c
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-09-22 12:55:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,3 +3,64 @@ class Cargo:
         self.weight = weight
 
 # write your code here
+
+
+class BaseRobot:
+    def __init__(self, name, weight, coords=None):
+        self.name = name
+        self.weight = weight
+        self.coords = coords
+        if self.coords is None:
+            self.coords = [0, 0]
+
+    def go_forward(self, step=1):
+        self.coords[1] += step
+        return self.coords
+
+    def go_back(self, step=1):
+        self.coords[1] -= step
+        return self.coords
+
+    def go_right(self, step=1):
+        self.coords[0] += step
+        return self.coords
+
+    def go_left(self, step=1):
+        self.coords[0] -= step
+        return self.coords
+
+    def get_info(self):
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name, weight, coords=None):
+        super().__init__(name, weight)
+        self.coords = coords
+        if self.coords is None:
+            self.coords = [0, 0, 0]
+
+    def go_up(self, step=1):
+        self.coords[2] += step
+        return self.coords
+
+    def go_down(self, step=1):
+        self.coords[2] -= step
+        return self.coords
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name, weight, max_load_weight, current_load,
+                 coords=None):
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = current_load
+
+    def hook_load(self, cargo):
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+        return self.current_load
+
+    def unhook_load(self):
+        self.current_load = None
+        return self.current_load
