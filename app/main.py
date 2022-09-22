95ed6619ff7efa89e6ea41b3d83c6a4f0dd3c63a
Edik Marushchak <e.moscow138@gmail.com>
2022-09-22 12:18:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ class BaseRobot:
     def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
 
 
