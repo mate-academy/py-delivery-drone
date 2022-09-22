5e30c2297286e48f4e22dae26435b163999c71fd
Stas Okhrym <s.okhrym@gmail.com>
2022-09-22 11:17:51 +0300

    Fixed func formating

--- app/main.py
+++ app/main.py
@@ -7,8 +7,7 @@ class BaseRobot:
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: list = None
-                 ):
+                 coords: list = None):
         self.name = name
         self.weight = weight
         self.coords = [0, 0] if coords is None else coords
@@ -33,8 +32,7 @@ class FlyingRobot(BaseRobot):
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: list = None
-                 ):
+                 coords: list = None):
         super(FlyingRobot, self).__init__(name, weight)
         self.coords = [0, 0, 0] if coords is None else coords
 
@@ -51,8 +49,7 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  current_load: int,
-                 coords: list = None
-                 ):
+                 coords: list = None):
         super(DeliveryDrone, self).__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
