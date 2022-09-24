03f8f458e58100a46aeafdc17a44fdb730040867
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-09-24 17:19:35 +0300

    Simplyfied adding step

--- app/main.py
+++ app/main.py
@@ -15,16 +15,16 @@ class BaseRobot:
             self.coords = coords
 
     def go_forward(self, step: int = 1) -> None:
-        self.coords[1] = self.coords[1] + step
+        self.coords[1] += step
 
     def go_back(self, step: int = 1) -> None:
-        self.coords[1] = self.coords[1] - step
+        self.coords[1] -= step
 
     def go_right(self, step: int = 1) -> None:
-        self.coords[0] = self.coords[0] + step
+        self.coords[0] += step
 
     def go_left(self, step: int = 1) -> None:
-        self.coords[0] = self.coords[0] - step
+        self.coords[0] -= step
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -40,10 +40,10 @@ class FlyingRobot(BaseRobot):
             self.coords = [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
-        self.coords[2] = self.coords[2] + step
+        self.coords[2] += step
 
     def go_down(self, step: int = 1) -> None:
-        self.coords[2] = self.coords[2] - step
+        self.coords[2] -= step
 
 
 class DeliveryDrone(FlyingRobot):
