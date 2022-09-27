7506e04bd85519eae8ec70b04deb6838895211c9
Illia Artnaiev <ilya.artnaev@gmail.com>
2022-09-27 09:14:43 +0300

    Better annotation and removed module

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-import typing
-
-
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
@@ -10,8 +7,7 @@ class BaseRobot:
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: typing.Optional[list[int]] = None
-                 ) -> None:
+                 coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords
@@ -38,8 +34,7 @@ class FlyingRobot(BaseRobot):
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: typing.Optional[list[int]] = None
-                 ) -> None:
+                 coords: list[int] = None) -> None:
         super(FlyingRobot, self).__init__(name, weight, coords)
         if self.coords == [0, 0]:
             self.coords = [0, 0, 0]
@@ -57,8 +52,7 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  current_load: Cargo,
-                 coords: typing.Optional[list[int]] = None
-                 ) -> None:
+                 coords: list[int] = None) -> None:
         super(DeliveryDrone, self).__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
