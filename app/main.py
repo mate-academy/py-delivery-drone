f3e485ad93ca810ddff746667a85b24dcc7838b0
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-21 14:17:31 +0300

    flake8 change

--- app/main.py
+++ app/main.py
@@ -56,5 +56,5 @@ class DeliveryDrone(FlyingRobot):
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
-    def unhook_load(self):
+    def unhook_load(self) -> None:
         self.current_load = None
