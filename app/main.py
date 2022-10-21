154899f12bbc90c5206cafa3cf77210f66879193
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-21 14:16:31 +0300

    added class DeliveryDrone with method

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Cargo:
     def __init__(self, weight):
         self.weight = weight
-        
+
 
 class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
@@ -41,3 +41,20 @@ class FlyingRobot(BaseRobot):
 
     def go_down(self, step):
         self.coords[2] -= step
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name: str,
+                 weight: int,
+                 max_load_weight: int = 0,
+                 current_load: int = None,
+                 coords : list = None) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = current_load
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+
+    def unhook_load(self):
+        self.current_load = None
