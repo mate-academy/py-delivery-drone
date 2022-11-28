76cab59cee38721f9da3bd6e16ec478691bb3d61
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-11-28 11:04:49 +0800

    Simplified writing condition for coords

--- app/main.py
+++ app/main.py
@@ -8,10 +8,9 @@ class Cargo:
 
 class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords = [0, 0] if coords is None else coords
         self.name = name
         self.weight = weight
-        self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -31,7 +30,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords = [0, 0, 0] if coords is None else coords
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
