f1b0ea558e31aa83d1edd2ecd154ce102d647bda
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-11-29 09:23:52 +0800

    DeliveryDrone __init__ fixed according to the best ptactices

--- app/main.py
+++ app/main.py
@@ -41,8 +41,14 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str, weight: int, max_load_weight: int,
-                 current_load: Cargo = None, coords: list = None,) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: Cargo = None,
+            coords: list = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
