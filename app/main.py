7bef04db713a885e34a53c93febfb2157d45759e
Vitalii Panochko <vpapplic@gmail.com>
2022-12-13 13:10:08 +0200

    rewrite in one line

--- app/main.py
+++ app/main.py
@@ -7,9 +7,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            coords = [0, 0]
-        self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -29,9 +27,8 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
-        super().__init__(name, weight, coords)
+        coords_value = [0, 0, 0] if coords is None else coords
+        super().__init__(name, weight, coords_value)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
