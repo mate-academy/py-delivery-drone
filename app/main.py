8a1eaa0d524b41ebcff6b41ca9fa07a2d59c3de5
Derkach Oleksandr <derkach03553@gmail.com>
2022-12-13 14:00:13 +0200

    fixed test

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Cargo:
 
 class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords = [0, 0] if coords is None else None
+        coords = [0, 0] if coords is None else coords
         self.name = name
         self.weight = weight
         self.coords = coords
@@ -28,7 +28,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords = [0, 0, 0] if coords is None else None
+        coords = [0, 0, 0] if coords is None else coords
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
