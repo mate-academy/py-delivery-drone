5319f66bf79833f49debeec1d152f2ac22d14615
Vitalii Panochko <vpapplic@gmail.com>
2022-12-15 02:45:54 +0200

    parent's constructor

--- app/main.py
+++ app/main.py
@@ -27,8 +27,11 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords_value = [0, 0, 0] if coords is None else coords
-        super().__init__(name, weight, coords_value)
+        super().__init__(
+            name=name,
+            weight=weight,
+            coords=[0, 0, 0] if coords is None else coords,
+        )
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
