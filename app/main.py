2afdb1f10eecc7745a70840d65fbf1d17bcc9248
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-10 14:44:07 +0200

    ifelse change inside init method

--- app/main.py
+++ app/main.py
@@ -10,12 +10,9 @@ class BaseRobot:
             weight: int,
             coords: list[int] = None
     ) -> None:
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
         self.name = name
         self.weight = weight
+        self.coords = coords if coords else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -40,8 +37,7 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list[int] = None
     ) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
+        coords = [0, 0, 0] if coords is None else coords
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
