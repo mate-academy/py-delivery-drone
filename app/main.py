222799816d7f905b4fcc430fc109351cf62997d2
Max Bilokur <bilmaxim@gmail.com>
2023-02-13 19:44:51 +0200

    init update

--- app/main.py
+++ app/main.py
@@ -28,8 +28,11 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
 
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords = [0, 0, 0] if coords is None else coords
-        super().__init__(name, weight, coords)
+        super().__init__(
+            name,
+            weight,
+            coords if coords is not None and len(coords) == 3 else [0, 0, 0]
+        )
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
