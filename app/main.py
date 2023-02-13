1693504e1140dbe9c099b98608b4cdfe36a6010a
Bohdan Kovalenko <bog7807@gmail.com>
2023-02-13 17:30:47 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,40 +2,41 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
+
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None):
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
         if not coords:
             coords = [0, 0]
         self.coords = coords
 
-    def go_forward(self, step: int = 1):
+    def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
 
-    def go_back(self, step: int = 1):
+    def go_back(self, step: int = 1) -> None:
         self.coords[1] -= step
 
-    def go_right(self, step: int = 1):
+    def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
 
-    def go_left(self, step: int = 1):
+    def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None):
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         super().__init__(name, weight, coords)
         if not coords:
             self.coords = [0, 0, 0]
 
-    def go_up(self, step: int = 1):
+    def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
 
-    def go_down(self, step: int = 1):
+    def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
 
 
