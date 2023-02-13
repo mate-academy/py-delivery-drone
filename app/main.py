14c65d1d193aa2cabb6e8a8b0562151fb4284af0
oithegod <serejathegod@gmail.com>
2023-02-13 15:48:38 +0200

    Hot fix

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
+class Cargo:
+    def __init__(
+            self,
+            weight: int | float
+    ) -> None:
+        self.weight = weight
+
+
 class BaseRobot:
     def __init__(self,
                  name: str,
@@ -25,14 +33,13 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 coords: list = None
-                 ) -> None:
-        super().__init__(name, weight, coords)
-
-        self.coords = [0, 0, 0] if coords is None else coords
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list = None
+    ) -> None:
+        super().__init__(name, weight, coords=coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -42,27 +49,21 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 max_load_weight: int,
-                 current_load: object,
-                 coords: list = None,
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: object,
+            coords: list = None
+    ) -> None:
         self.max_load_weight = max_load_weight
         self.current_load = current_load
         super().__init__(name, weight, coords)
 
-    def hook_load(self, cargo: object) -> None:
+    def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
     def unhook_load(self) -> None:
         self.current_load = None
-
-
-class Cargo:
-    def __init__(self,
-                 weight: int | float
-                 ) -> None:
-        self.weight = weight
