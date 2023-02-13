0364916ba3e5f4a7ef13677d827803b2fefd81fd
Yulia <Mala>
2023-02-13 18:52:26 +0200

    current_load annotation fixed. default coords are passed right into the super init

--- app/main.py
+++ app/main.py
@@ -27,8 +27,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        coords = coords or [0, 0, 0]
-        super().__init__(name, weight, coords)
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -42,7 +41,7 @@ class DeliveryDrone(FlyingRobot):
                  name: str,
                  weight: int,
                  max_load_weight: int,
-                 current_load: int,
+                 current_load: int | None,
                  coords: list = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
