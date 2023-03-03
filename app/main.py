13b4415eb47ebcebcbdbc0339f9ace2290a98577
Andrii Fedoryshyn <warvar2016wgmail.com>
2023-03-03 20:25:07 +0200

    Solution_for_robots

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
+from typing import Optional
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: Optional[list] = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords or [0, 0]
@@ -26,7 +33,11 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: Optional[list] = None) -> None:
         super().__init__(name, weight)
 
         self.coords = coords or [0, 0, 0]
@@ -45,12 +56,12 @@ class DeliveryDrone(FlyingRobot):
             weight: int,
             coords: list = None,
             max_load_weight: int = 0,
-            current_load: Cargo = None) -> None:
+            current_load: Optional[Cargo] = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
-    def hook_load(self, cargo: Cargo) -> None:
+    def hook_load(self, cargo: Optional[Cargo]) -> None:
         if cargo.weight <= self.max_load_weight and self.current_load is None:
             self.current_load = cargo
 
