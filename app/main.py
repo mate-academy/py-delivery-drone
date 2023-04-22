6839ef4d371e277763a0a29e7284dee7d1171308
Nikita Provorov <nikiyorkovich@gmail.com>
2023-04-22 12:24:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
@@ -5,7 +8,7 @@ class Cargo:
 
 class BaseRobot:
     def __init__(self, name: str, weight: int,
-                 coords: list[int] = None) -> None:
+                 coords: Optional[list[int]] = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -28,8 +31,8 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int,
-                 coords: list[int] = None) -> None:
-        super().__init__(name, weight)
+                 coords: Optional[list[int]] = None) -> None:
+        super().__init__(name, weight, coords)
         self.coords = coords if coords is not None else [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
@@ -43,8 +46,8 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str, weight: int,
                  max_load_weight: int,
                  current_load: int,
-                 coords: list[int] = None) -> None:
-        super().__init__(name, weight)
+                 coords: Optional[list[int]] = None) -> None:
+        super().__init__(name, weight, coords)
         self.coords = coords if coords is not None else [0, 0, 0]
         self.max_load_weight = max_load_weight
         self.current_load = current_load
