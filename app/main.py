d1e1309d471eb5c750d7578365a5f9487098dd5a
Dmitry <dimior1992@gmail.com>
2023-04-28 15:52:34 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -13,10 +13,7 @@ class BaseRobot:
                  coords: Optional[list[int]] = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
