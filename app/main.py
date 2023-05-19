62a713f4508db43f5118619f95431240bdf6e4a9
Arina Oltarzhevska <arinaoltarzhevska.ua@gmail.com>
2023-05-19 20:08:21 +0200

    change 4.0

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class BaseRobot:
                  coords: Optional[list[int]] = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = [0, 0] if coords is None else coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
