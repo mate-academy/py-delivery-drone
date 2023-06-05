bcb16b949294dff01be80bdbdc5d3d79f037d9f0
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-05 17:24:20 +0300

    Simplified comparison, code is made DRY, arguments are written per line

--- app/main.py
+++ app/main.py
@@ -8,10 +8,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -32,10 +29,8 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
 
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            super().__init__(name, weight, coords=[0, 0, 0])
-        else:
-            super().__init__(name, weight, coords)
+        coords = coords or [0, 0, 0]
+        super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -46,8 +41,13 @@ class FlyingRobot(BaseRobot):
 
 class DeliveryDrone(FlyingRobot):
 
-    def __init__(self, name: str, weight: int, max_load_weight: int,
-                 coords: list = None, current_load: Cargo = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 max_load_weight: int,
+                 coords: list = None,
+                 current_load: Cargo = None) -> None:
+
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
