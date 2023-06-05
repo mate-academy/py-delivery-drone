a25ad074e7bf8c5bcf729473df5f6abfb5bb1a4d
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-06-05 17:07:34 +0300

    fixed item assignment for 'coords' and type annotation for current load

--- app/main.py
+++ app/main.py
@@ -5,13 +5,10 @@ class Cargo:
 
 class BaseRobot:
 
-    def __init__(self, name: str, weight: 34, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -32,11 +29,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
 
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -51,7 +44,7 @@ class DeliveryDrone(FlyingRobot):
                  name: str,
                  weight: int,
                  max_load_weight: int,
-                 current_load: int,
+                 current_load: Cargo,
                  coords: list = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
