948edfc2cc0bf13c70c0be091ec92b14f5eaeb76
Vlad <vladislav.zhyla@gmail.com>
2023-06-05 21:59:23 +0300

    Coords init simplified. Fixed type annotations

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -37,8 +37,7 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list = None
     ) -> None:
-        super().__init__(name, weight)
-        self.coords = coords if coords else [0, 0, 0]
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -52,7 +51,7 @@ class DeliveryDrone(FlyingRobot):
             self,
             name: str,
             weight: int,
-            max_load_weight: int,
+            max_load_weight: [int, float],
             coords: list = None,
             current_load: int = None
     ) -> None:
