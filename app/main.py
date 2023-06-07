456b7183eb544e9f96dfc117366c1f3038d0d762
Vlad <vladislav.zhyla@gmail.com>
2023-06-07 12:33:51 +0300

    Fixed type annotations for current_load

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ class DeliveryDrone(FlyingRobot):
             weight: int,
             max_load_weight: [int, float],
             coords: list = None,
-            current_load: int = None
+            current_load: "Cargo" = None
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
