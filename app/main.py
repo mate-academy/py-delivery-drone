27526346230a56cc111d7614336aa25e7d973650
mark.shapka <mark.shapka1@gmail.com>
2023-06-08 15:30:19 +0300

    added type Optional

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 Coord = int
 
 
@@ -9,7 +10,7 @@ class Cargo:
 class BaseRobot:
     def __init__(self, name: str,
                  weight: int,
-                 coords: list[Coord] = None) -> None:
+                 coords: Optional[list[Coord]] = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -33,7 +34,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str,
                  weight: int,
-                 coords: list[Coord] = None) -> None:
+                 coords: Optional[list[Coord]] = None) -> None:
         super().__init__(name, weight, coords)
         self.coords = coords if coords is not None else [0, 0, 0]
 
@@ -48,8 +49,8 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str,
                  weight: int,
                  max_load_weight: int,
-                 coords: list[Coord] = None,
-                 current_load: Cargo = None) -> None:
+                 coords: Optional[list[Coord]] = None,
+                 current_load: Optional[Cargo] = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
