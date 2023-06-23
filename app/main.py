e71b25643fb52e1f01c82373f11187809a8046d9
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-06-23 21:12:10 +0200

    correction 3

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class BaseRobot:
                  coords: Optional[list[int]] = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -35,7 +35,7 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int,
                  coords: Optional[list[int]] = None) -> None:
         super().__init__(name, weight)
-        self.coords = coords if coords else [0, 0, 0]
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
