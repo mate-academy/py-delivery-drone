9ec66e4b2dc4988f5429440b7d16242e9275c6fc
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-06-29 13:37:27 +0300

    Solution by Alex

--- app/main.py
+++ app/main.py
@@ -7,11 +7,12 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 coords: Optional[list[int]] = None
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: Optional[list[int]] = None
+    ) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords else [0, 0]
@@ -33,11 +34,12 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 coords: Optional[list[int]] = None
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: Optional[list[int]] = None
+    ) -> None:
         coords = coords if coords else [0, 0, 0]
         super().__init__(name, weight, coords)
 
@@ -49,13 +51,14 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 max_load_weight: int,
-                 current_load: None,
-                 coords: Optional[list[int]] = None
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: Optional[Cargo] = None,
+            coords: Optional[list[int]] = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
