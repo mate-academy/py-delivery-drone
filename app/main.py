fc65f500cde49063412f48f09e54a8589585a4fa
Vladyslav Shepilov <shepsterya@gmail.com>
2023-07-02 23:07:29 +0300

    added 'or' statment

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ class BaseRobot:
                  coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -34,8 +31,7 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str,
                  weight: int,
                  coords: list[int] = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
