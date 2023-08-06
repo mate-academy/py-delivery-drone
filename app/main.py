11a3f79320407bdb8642b6c654846d13b6774691
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-08-06 16:48:55 +0200

    Refactored

--- app/main.py
+++ app/main.py
@@ -12,10 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -40,14 +37,8 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list = None
     ) -> None:
-        if coords is None:
-            super().__init__(name, weight)
-            self.coords = [0, 0, 0]
-        elif len(coords) == 2:
-            super().__init__(name, weight, coords)
-        else:
-            super().__init__(name, weight)
-            self.coords = coords
+        super().__init__(name, weight)
+        self.coords = [0, 0, 0] if coords is None else coords
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -65,14 +56,7 @@ class DeliveryDrone(FlyingRobot):
             current_load: Cargo = None,
             coords: list = None
     ) -> None:
-        if coords is None:
-            super().__init__(name, weight)
-            self.coords = [0, 0, 0]
-        elif len(coords) == 2:
-            super().__init__(name, weight, coords)
-        else:
-            super().__init__(name, weight)
-            self.coords = coords
+        super().__init__(name, weight, coords)
 
         self.max_load_weight = max_load_weight
         self.current_load = current_load
