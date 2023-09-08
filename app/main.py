c7dd061d036eb22516c35c9087783db1bd1e2a09
Anton Tkachuk <tkacuk2291@gmail.com>
2023-09-09 00:28:53 +0300

    Solutions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,58 @@
+from typing import Optional
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    def __init__(self, name: str, weight: int,
+                 coords: Optional[list] = None) -> None:
+        self.name = name
+        self.weight = weight
+        self.coords = coords if coords is not None else [0, 0]
+
+    def go_forward(self, step: int = 1) -> None:
+        self.coords[1] += step
+
+    def go_back(self, step: int = 1) -> None:
+        self.coords[1] -= step
+
+    def go_right(self, step: int = 1) -> None:
+        self.coords[0] += step
+
+    def go_left(self, step: int = 1) -> None:
+        self.coords[0] -= step
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int,
+                 coords: Optional[list] = None) -> None:
+        super().__init__(name, weight, coords)
+        self.coords = coords if coords is not None else [0, 0, 0]
+
+    def go_up(self, z_coordinate: int = 1) -> None:
+        self.coords[2] += z_coordinate
+
+    def go_down(self, z_coordinate: int = 1) -> None:
+        self.coords[2] -= z_coordinate
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name: str, weight: int, max_load_weight: int,
+                 current_load: Optional[Cargo] = None,
+                 coords: Optional[list] = None) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = current_load
+
+    def hook_load(self, obj: Cargo) -> None:
+        if self.current_load is None and obj.weight <= self.max_load_weight:
+            self.current_load = obj
+
+    def unhook_load(self) -> None:
+        self.current_load = None
