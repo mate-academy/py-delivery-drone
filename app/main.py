fcbcaaf7ac70e85b89d1113de64d754f7ce77f1e
Andrii Hrabovyi <mrmestage25@gmail.com>
2023-09-22 18:30:46 +0300

    latest edits

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ class DeliveryDrone(FlyingRobot):
             current_load: int = None,
             coords : list[int] = None
     ) -> None:
-        super().__init__(name, weight, coords)
+        super().__init__(name=name, weight=weight, coords=coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
