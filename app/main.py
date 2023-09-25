69eae6570e062024474da00c97b75dcc3af4ac78
Oleksiy Rozehan <rozeganalexey@gmail.com>
2023-09-25 16:59:51 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -5,10 +5,15 @@ class Cargo:
 
 class BaseRobot:
 
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[tuple] = None
+    ) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -28,11 +33,17 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
 
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[tuple] = None
+    ) -> None:
         super().__init__(
             name=name,
             weight=weight,
-            coords=coords if coords is not None else [0, 0, 0])
+            coords=coords or [0, 0, 0]
+        )
 
     def go_up(self, coordinate_z: int = 1) -> None:
         self.coords[2] += coordinate_z
@@ -42,16 +53,19 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 max_load_weight: int,
-                 current_load: int,
-                 coords: list = None,
-                 ) -> None:
-        super().__init__(name=name,
-                         weight=weight,
-                         coords=coords if coords is not None else [0, 0, 0])
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: int,
+            coords: list[tuple] = None,
+    ) -> None:
+        super().__init__(
+            name=name,
+            weight=weight,
+            coords=coords or [0, 0, 0]
+        )
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
