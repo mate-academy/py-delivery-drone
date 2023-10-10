dd7c769c242695f088b3b11c669443aa5fe9ab6e
Denys Kibalenko <dennieking87@gmail.com>
2023-10-10 14:12:41 +0200

    Solution fix init condition

--- app/main.py
+++ app/main.py
@@ -41,10 +41,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if not coords:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         """
