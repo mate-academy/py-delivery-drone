c55e12a9adc5ebc5a377e31d0aef4dfbeb1039c0
Serhii <martinenko.sergiy@gmail.com>
2023-10-11 16:52:52 +0300

    type annotations cargo | none

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ class Cargo:
         self.weight = weight
 
 
-class BaseRobot():
+class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
@@ -41,11 +41,10 @@ class FlyingRobot(BaseRobot):
 class DeliveryDrone(FlyingRobot):
     def __init__(self,
                  max_load_weight: int,
-                 current_load: int,
+                 current_load: Cargo | None,
                  name: str,
                  weight: int,
-                 coords: list = None
-                 ) -> None:
+                 coords: list = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
