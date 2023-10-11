21d5ed99426917f5a7591a09a778ef9d18187f33
Ihor Prochnickiy <2prochigor@gmail.com>
2023-10-11 12:26:19 +0300

    use a single composite if statement

--- app/main.py
+++ app/main.py
@@ -8,8 +8,7 @@ class BaseRobot:
         self.name = name
         self.weight = weight
         self.coords = coords
-        if self.coords is None:
-            self.coords = [0, 0]
+        self.coords = coords if self.coords else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
