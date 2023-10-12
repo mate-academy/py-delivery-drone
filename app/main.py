5ad29c3007b3a8a4a0503cd1e264208c4038b70f
Frezworx <frezworx@gmail.com>
2023-10-12 17:49:44 +0300

    Fix: use ternary operator in constructor [class FlyingRobot] and [class BaseRobot]

--- app/main.py
+++ app/main.py
@@ -12,10 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -36,8 +33,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
 
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
+        coords = [0, 0, 0] if coords is None else coords
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
