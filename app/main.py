69a55e5ddf0323062712e09837d1c1bf84559302
Hryhoriy Bilous <hryhoriy.bilous@gmail.com>
2023-10-17 15:37:59 +0300

    Solution amazon2

--- app/main.py
+++ app/main.py
@@ -4,10 +4,13 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords else [0, 0]
+        self.coords = coords if type(coords) == list else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -26,8 +29,13 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords if coords else [0, 0, 0])
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] = None) -> None:
+        super().__init__(name,
+                         weight,
+                         coords if type(coords) == list else [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -42,8 +50,10 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  current_load: Cargo,
-                 coords: list = None) -> None:
-        super().__init__(name, weight, coords if coords else [0, 0, 0])
+                 coords: list[int] = None) -> None:
+        super().__init__(name,
+                         weight,
+                         coords if type(coords) == list else [0, 0, 0])
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
