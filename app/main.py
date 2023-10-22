0a29207e393b91c1e092a1c4edf5c761febfc44b
Yuliia Dudych <juliadudych@gmail.com>
2023-10-22 23:37:10 +0300

    Define BaseRobot class

--- app/main.py
+++ app/main.py
@@ -2,4 +2,32 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] | None = None
+    ) -> None:
+
+        self.name = name
+        self.weight = weight
+        if not coords:
+            coords = [0, 0]
+        self.coords = coords[:2]
+
+    def go_forward(self, step: int = 1) -> None:
+        self.coords[1] += step
+
+    def go_back(self, step: int = 1) -> None:
+        self.coords[1] -= step
+
+    def go_right(self, step: int = 1) -> None:
+        self.coords[0] += step
+
+    def go_left(self, step: int = 1) -> None:
+        self.coords[0] -= step
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
