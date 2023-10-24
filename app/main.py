398ec7fbb51a7f081d1a3b40fa3ef071db7659b2
Artem Poltavets <109797006+Artem-user-19@users.noreply.github.com>
2023-10-24 19:27:38 +0300

    Update app/main.py
    Co-authored-by: Dmytro Skoryi <76879254+Dimosphen1@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ class FlyingRobot(BaseRobot):
             coords: Optional[List[int]] = None) -> None:
         if coords is None:
             coords = [0, 0, 0]
-        super().__init__(name, weight, coords)
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
