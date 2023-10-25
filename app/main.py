a9dbe2b2b2dd9b2d313f3ccb987ca069517a291c
Anatolii Hrechaniuk <genezisium@gmail.com>
2023-10-25 16:39:48 +0300

    deleted conditions and changed annotations

--- app/main.py
+++ app/main.py
@@ -4,10 +4,14 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] = None
+                 ) -> None:
         self.name = name
         self.weight = weight
-        self.coords = [0, 0] if coords is None else coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -26,11 +30,15 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] = None
+                 ) -> None:
         super().__init__(
             name,
             weight,
-            coords=[0, 0, 0] if coords is None else coords
+            coords=coords or [0, 0, 0]
         )
 
     def go_up(self, step: int = 1) -> None:
@@ -46,7 +54,7 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  current_load: int = None,
-                 coords: list = None
+                 coords: list[int] = None
                  ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
