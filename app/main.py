57f0f94631fca6cafa813ecab2f6353a8c0f5a3d
Marinel444 <380978907444m@gmail.com>
2023-11-02 16:01:48 +0200

    solv flake8

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Cargo:
 
 # write your code here
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords=None) -> None:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords
@@ -29,7 +29,7 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords=None) -> None:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         super().__init__(name, weight, coords)
         if coords is None:
             self.coords = [0, 0, 0]
