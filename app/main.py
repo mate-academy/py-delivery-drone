df25aecf456495e3da44269556b35382cc29bea3
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-11-13 19:48:38 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ from typing import List, Optional
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: float, coords: Optional[List[float]] = None) -> None:
+    def __init__(
+        self,
+        name: str,
+        weight: float,
+        coords: Optional[List[float]] = None
+    ) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -29,7 +34,12 @@ class Cargo:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: float, coords: Optional[List[float]] = None) -> None:
+    def __init__(
+        self,
+        name: str,
+        weight: float,
+        coords: Optional[List[float]] = None
+    ) -> None:
         if coords is None:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
@@ -42,8 +52,14 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str, weight: float, max_load_weight: float,
-                 current_load: Optional[Cargo], coords: Optional[List[float]] = None) -> None:
+    def __init__(
+        self,
+        name: str,
+        weight: float,
+        max_load_weight: float,
+        current_load: Optional[Cargo],
+        coords: Optional[List[float]] = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
