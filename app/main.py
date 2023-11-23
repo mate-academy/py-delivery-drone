859b5f64f6b6477fd3069e18b609f56c46626a98
Ryshtun Denys <rishtund@gmail.com>
2023-11-23 12:26:04 +0200

    JDone (Il)

--- app/main.py
+++ app/main.py
@@ -4,12 +4,16 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] = None
+    ) -> None:
+
         self.name = name
         self.weight = weight
-        self.coords = coords
-        if not coords:
-            self.coords = [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -28,10 +32,15 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords)
-        if not coords:
-            self.coords.append(0)
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] = None
+    ) -> None:
+
+        super().__init__(name, weight)
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -47,10 +56,11 @@ class DeliveryDrone(FlyingRobot):
             weight: int,
             max_load_weight: int = 0,
             current_load: bool = None,
-            coords: list = None
+            coords: list[int] = None
     ) -> None:
 
-        super().__init__(name, weight, coords)
+        super().__init__(name, weight)
+        self.coords = coords or [0, 0, 0]
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
