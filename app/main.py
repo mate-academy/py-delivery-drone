594612e2afb9989497104d36f438fcdfd956217e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-11-28 16:08:55 +0200

    Added list element type annotation

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int, coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords or [0, 0]
@@ -26,7 +26,7 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int, coords: list[int] = None) -> None:
         super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
@@ -42,7 +42,7 @@ class DeliveryDrone(FlyingRobot):
                  current_load: bool,
                  name: str,
                  weight: int,
-                 coords: list = None,
+                 coords: list[int] = None,
                  ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
