091d38cb1d99738a0d921ed0fc9cf0e209322997
Sergey <rinoto.kun@gmail.com>
2023-11-29 00:09:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,10 +10,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords:
-            self.coords = coords
-        else:
-            self.coords = [0, 0]
+        self.coords = coords or [0, 0]
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -33,10 +30,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords:
-            super().__init__(name, weight, coords)
-        else:
-            super().__init__(name, weight, coords=[0, 0, 0])
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
