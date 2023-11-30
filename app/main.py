ba13edbf0caea6f1167269ea7698c16e871ab696
Maksym Bondar <maks.bondar878@gmail.com>
2023-11-30 16:32:57 +0200

    New Solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 coords: float = None) -> None:
-        self. name = name
-        self. weight = weight
-        if coords is None:
-            coords = [0, 0]
-        self. coords = coords
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list = None
+    ) -> None:
+        self.name = name
+        self.weight = weight
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -35,33 +35,36 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 coords: float = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list = None
+    ) -> None:
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
-        self.coords[2] += step  # Increase Z coordinate to go up
+        self.coords[2] += step
 
     def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 coords: int = None,
-                 max_load_weight: int = None,
-                 current_load: bool = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list = None,
+            max_load_weight: int = None,
+            current_load: bool = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
-    def hook_load(self, cargo: int) -> None:
+    def hook_load(self, cargo: Cargo) -> None:
         if (self.current_load is None
                 and cargo.weight <= self.max_load_weight):
             self.current_load = cargo
