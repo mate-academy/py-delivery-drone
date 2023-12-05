e9983f7a656dbc4b9aee38c769106140703bb84a
Misha Purh <mishapurh@Mishas-MacBook-Air.local>
2023-12-04 20:52:19 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ class Cargo:
 class BaseRobot:
     robot = []
 
-    def __init__(self, name: str, weight: int, coords: list[int] | None = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] | None = None
+    ) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -28,7 +33,12 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list[int] | None = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] | None = None
+    ) -> None:
         if coords is None:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
