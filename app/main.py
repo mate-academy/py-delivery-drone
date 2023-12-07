9ff951ed8b25aca7acd53b877f99fe26d6726e3b
Olha Rozdorozhna <olga.rozdorozhna@gmail.com>
2023-12-07 14:11:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -17,21 +17,17 @@ class BaseRobot:
         self.weight = weight
         self.coords = coords if coords else [0, 0]
 
-    def go_forward(self, step: int = 1) -> list:
+    def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
-        return self.coords
 
-    def go_back(self, step: int = 1) -> list:
+    def go_back(self, step: int = 1) -> None:
         self.coords[1] -= step
-        return self.coords
 
-    def go_right(self, step: int = 1) -> list:
+    def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
-        return self.coords
 
-    def go_left(self, step: int = 1) -> list:
+    def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
-        return self.coords
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -47,13 +43,11 @@ class FlyingRobot(BaseRobot):
         super().__init__(name, weight, coords)
         self.coords = coords if coords else [0, 0, 0]
 
-    def go_up(self, step: int = 1) -> list:
+    def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
-        return self.coords
 
-    def go_down(self, step: int = 1) -> list:
+    def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
-        return self.coords
 
 
 class DeliveryDrone(FlyingRobot):
