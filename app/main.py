0194d987a72fb4d4804a12860a8d632f29c5b2bf
Dariia Shostak <davorobii@gmail.com>
2023-12-22 13:11:38 +0200

    fix solution

--- app/main.py
+++ app/main.py
@@ -7,25 +7,19 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            coords = [0, 0]
-        self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
-    def go_forward(self, step: int = 1) -> list:
+    def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
-        return self.coords
 
-    def go_back(self, step: int = 1) -> list:
+    def go_back(self, step: int = 1) -> None:
         self.coords[1] -= step
-        return self.coords
 
-    def go_right(self, step: int = 1) -> list:
+    def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
-        return self.coords
 
-    def go_left(self, step: int = 1) -> list:
+    def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
-        return self.coords
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -33,14 +27,10 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords)
-        if coords is None:
-            coords = [0, 0, 0]
-        self.coords = coords
+        super().__init__(name=name, weight=weight, coords=coords or [0, 0, 0])
 
-    def go_up(self, step: int = 1) -> list:
+    def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
-        return self.coords
 
     def go_down(self, step: int = 1) -> list:
         self.coords[2] -= step
