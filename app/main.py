78b36f4b6c7016456f7938f15496ee4336455565
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-12-29 16:12:06 +0100

    fix task

--- app/main.py
+++ app/main.py
@@ -28,7 +28,8 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list[int] = None) -> None:
+    def __init__(self, name: str, weight: int,
+                 coords: list[int] = None) -> None:
         coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
