d6915e7cc867ed23339a9896e30263fd78f80ed5
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-04 17:23:04 +0200

    fix annotation and indentation

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-from typing import Callable
-
-
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] = None
+    ) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords else [0, 0]
@@ -29,7 +31,12 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] = None
+    ) -> None:
         super().__init__(name, weight)
         self.coords = coords if coords else [0, 0, 0]
 
@@ -41,13 +48,19 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str, weight: int, max_load_weight: int,
-                 current_load: Callable, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: Cargo,
+            coords: list[int] = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
-    def hook_load(self, cargo: Callable) -> None:
+    def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and self.max_load_weight >= cargo.weight:
             self.current_load = cargo
 
