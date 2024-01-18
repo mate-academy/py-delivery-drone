098c0e78253025c743ce003aa612615b0caf31b4
Ihor Bondarenko <igorb6766@gmail.com>
2024-01-18 09:40:45 +0200

    fixed annotations

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int, coords: None = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = [0, 0] if not coords else coords
@@ -26,9 +26,8 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if not coords:
-            coords = [0, 0, 0]
+    def __init__(self, name: str, weight: int, coords: None = None) -> None:
+        coords = [0, 0, 0] if not coords else coords
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
@@ -45,7 +44,7 @@ class DeliveryDrone(FlyingRobot):
             weight: int,
             max_load_weight: int,
             current_load: int,
-            coords: list = None
+            coords: None = None
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
