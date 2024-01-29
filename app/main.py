cfbaabd67086bc72fcd2a0d23d72378d6059622a
voidmain69 <photoreznichenko@gmail.com>
2024-01-29 16:54:22 +0200

    fix annotations

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Cargo:
 
 class BaseRobot:
     def __init__(
-            self, name: str, weight: int, coords: list[int] = None
+            self, name: str, weight: int, coords: list[int] | None = None
     ) -> None:
         self.name = name
         self.weight = weight
@@ -29,7 +29,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(
-            self, name: str, weight: int, coords: list[int] = None
+            self, name: str, weight: int, coords: list[int] | None = None
     ) -> None:
         super().__init__(name, weight, coords or [0, 0, 0])
 
@@ -43,7 +43,7 @@ class FlyingRobot(BaseRobot):
 class DeliveryDrone(FlyingRobot):
     def __init__(
             self, name: str, weight: int, max_load_weight: int,
-            current_load: int, coords: list[int] = None
+            current_load: int, coords: list[int] | None = None
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
