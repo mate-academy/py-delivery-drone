653f439eed602abe2d4b7e684031ebe4a4d34b13
Mikola Rozhansky <bymikola@gmail.com>
2024-02-20 23:04:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,4 +2,12 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
- write your code here
+
+class BaseRobot:
+    def __init__(self, name:str, weight: int, coords: list = None):
+        self.name = name
+        self.weight = weight
+        self.coords = coords[0], coords[1]
+
+
+
