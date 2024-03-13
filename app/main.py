a99d10ff3020f9de20b483ce9627b51fa432a0f5
bujhm <Bujhm99@gmail.com>
2024-03-13 20:40:00 +0200

    args in line, one line if

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if not coords else coords
+
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -44,15 +42,17 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str, weight: int,
-                 max_load_weight: int, current_load: Cargo = None,
+    def __init__(self, name: str,
+                 weight: int,
+                 max_load_weight: int,
+                 current_load: Cargo = None,
                  coords: list = None, ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load is None and cargo.weight <= self.max_load_weight:
+        if not self.current_load and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
     def unhook_load(self) -> None:
