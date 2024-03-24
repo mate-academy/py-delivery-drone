27c89a980dc8cd0d1b286ed57162d4c5d4bf01cc
NioviIki <charmniovi@tutanota.com>
2024-03-24 17:21:09 +0200

    added BaseRobot

--- app/main.py
+++ app/main.py
@@ -2,4 +2,25 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot(Cargo):
+    def __init__(self, name: str, weight: int, coords: list[int, int] = None) -> None:
+        super().__init__(weight)
+        self.name = name
+        if not coords:
+            self.coords = [0, 0]
+
+    def go_forward(self, parameter: int = 1):
+        self.coords[1] += parameter
+
+    def go_back(self, parameter: int = 1):
+        self.coords[1] -= parameter
+
+    def go_right(self, parameter: int = 1):
+        self.coords[0] += parameter
+
+    def go_left(self, parameter: int = 1):
+        self.coords[0] -= parameter
+
+    def get_info(self):
+        return f"Robot: {self.name}, Weight: {self.weight}"
