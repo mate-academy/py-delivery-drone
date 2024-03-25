08ac678fa049f2fc1cbf1c3ffcb587a244e2c50f
Andrii Chubenko <stalkerock@gmail.com>
2024-03-25 10:22:37 +0200

    Solving

--- app/main.py
+++ app/main.py
@@ -7,8 +7,7 @@ class BaseRobot:
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: list[int] = None
-                 ) -> None:
+                 coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords or [0, 0]
@@ -31,11 +30,9 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int,
-                 coords: list = None
-                 ) -> None:
+                 coords: list = None) -> None:
 
-        coords = coords or [0, 0, 0]
-        super().__init__(name, weight, coords)
+        super().__init__(name, weight, coords=coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
