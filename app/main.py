1593931d1ed630bccb8a033e81fb074873ac42a8
semermaks <semermaks@mail.com>
2024-03-25 22:46:22 +0200

    Shorter coords case, current_load to Cargo obj

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -27,7 +27,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        self.coords = coords if coords is not None else [0, 0, 0]
+        self.coords = coords or [0,0,0]
         super().__init__(name, weight, self.coords)
 
     def go_up(self, step: int = 1) -> None:
@@ -41,7 +41,7 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str,
                  weight: int,
                  max_load_weight: int,
-                 current_load: int,
+                 current_load: Cargo,
                  coords: list = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
