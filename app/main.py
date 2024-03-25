f0187b757d69be52b9978a4b7e9d750d8d1eb9c8
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-25 15:50:11 +0200

    Fixed annotation

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ class DeliveryDrone(FlyingRobot):
                  name: str,
                  weight: int,
                  max_load_weight: int,
-                 current_load: int | None,
+                 current_load: Cargo | None,
                  coords: list = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
