b254c82b4f72455305c4790ab08076ff7db596ca
semermaks <semermaks@mail.com>
2024-03-26 11:49:33 +0200

    fix overwrite attr in FlyingRobot init

--- app/main.py
+++ app/main.py
@@ -27,8 +27,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        self.coords = coords or [0, 0, 0]
-        super().__init__(name, weight, self.coords)
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
