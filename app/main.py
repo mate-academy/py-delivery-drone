8c0912bdb6a24e0a4cffc62b3c87da73160dc58c
bwaah1 <chykivan@gmail.com>
2024-03-27 10:50:25 +0200

    Fix the issue with coords

--- app/main.py
+++ app/main.py
@@ -7,9 +7,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            coords = [0, 0]
-        self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -31,9 +29,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         super().__init__(name, weight)
-        if coords is None:
-            coords = [0, 0, 0]
-        self.coords = coords[:3]
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -46,15 +42,15 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self,
                  name: str,
                  weight: int,
+                 current_load: Cargo,
                  coords: list = None,
-                 max_load_weight: int = 0,
-                 current_load: Cargo = None) -> None:
+                 max_load_weight: int = 0) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load is None and cargo.weight <= self.max_load_weight:
+        if not self.current_load and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
     def unhook_load(self) -> None:
