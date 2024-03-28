5ea339a3b37312e5a5d694b6a0fa028bb6c50867
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-03-28 17:13:59 +0200

    used ternary operator

--- app/main.py
+++ app/main.py
@@ -10,10 +10,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if not coords:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords if coords else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -33,10 +30,8 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if not coords:
+        super().__init__(name, weight, coords) if coords else \
             super().__init__(name, weight, coords=[0, 0, 0])
-        else:
-            super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
