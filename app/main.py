06e966619cd0aa6e7a443698426d7ce55590881b
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-03-29 21:45:04 -0600

    delete useless code inside FlyingRobot class

--- app/main.py
+++ app/main.py
@@ -39,10 +39,7 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list[int] = None
     ) -> None:
-        if not coords:
-            super().__init__(name, weight, [0, 0, 0])
-        else:
-            super().__init__(name, weight, coords)
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
