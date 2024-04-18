4fd7a0d2a90d06820d6d2855078758a716ae57fb
Olena Poplias <poplyasitasony@gmail.com>
2024-04-18 14:22:01 -0400

    "Solution"

--- app/main.py
+++ app/main.py
@@ -27,7 +27,6 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        # super().__init__(name, weight, coords)
         super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
