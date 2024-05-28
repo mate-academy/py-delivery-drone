91e48d90445cd8f14fdfacd6c5003fd60f3369b2
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-05-28 12:10:53 +0300

    fix type annotation for attribute coords and optimize condition

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ class BaseRobot:
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: list[int] = None) -> None:
+                 coords: list[int] | None = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -33,10 +33,10 @@ class FlyingRobot(BaseRobot):
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: list = None) -> None:
+                 coords: list[int] | None = None) -> None:
         super().__init__(name, weight, coords)
 
-        self.coords = coords if coords is not None else [0, 0, 0]
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -52,7 +52,7 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  current_load: Cargo = None,
-                 coords: list = None) -> None:
+                 coords: list[int] | None = None) -> None:
 
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
