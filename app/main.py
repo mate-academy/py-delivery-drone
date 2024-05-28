521d6ff87d8fdd12c0e27abd2c8f4e95ac610df7
MaglorFeanorson <157366510+MaglorFeanorson@users.noreply.github.com>
2024-05-28 23:13:39 +0300

    "SolutionV2"

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -28,10 +28,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
-        elif len(coords) == 2:
-            coords.append(0)
+        coords = coords or [0, 0, 0]  # Simplified initialization for 3D coords
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
