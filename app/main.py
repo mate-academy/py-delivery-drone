794b2efc31fb0dbc5c355f2d8438bd26afeb0662
DimaSt <82927379+dmytrostepanenko@users.noreply.github.com>
2024-05-29 10:12:47 +0300

    Update app/main.py
    Co-authored-by: Eduard Alekseiev <edikaleks43+github@gmail.com>

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int, coords: list[int] | None = None) -> None:
 
         if coords is None:
             coords = [0, 0]
