8635a6bf7e2292691690a533ec636c1d57e1f369
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-07-03 23:50:11 +0300

    new sol 2

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ class FlyingRobot(BaseRobot):
                  weight: int,
                  coords: list[int] = None) -> None:
         super().__init__(name, weight, coords)
-            self.coords = coords or [0, 0]
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step : int = 1) -> None:
         self.coords[2] += step
