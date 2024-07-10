fd8064141a2d994d8bca4ccd4f097cd519d2ff86
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-10 20:01:27 +0300

    Changed the way of passing undefined variable "coords" to constructor

--- app/main.py
+++ app/main.py
@@ -38,8 +38,7 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: List[int] = None
     ) -> None:
-        coords = [0, 0, 0] if coords is None else coords
-        super().__init__(name, weight, coords)
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
