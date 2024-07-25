e758c1a920d093fba0834d2ef1d05b2b9eb5d984
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-25 15:46:05 +0300

    ternary operator has been used instead of regular if/else

--- app/main.py
+++ app/main.py
@@ -8,10 +8,7 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords if coords else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
