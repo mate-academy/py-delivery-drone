2b42ff12fccc832e4715fefe4d91b6739a323b9f
Krasul1a <42385512+Krasul1a@users.noreply.github.com>
2024-07-25 16:50:19 +0300

    Update app/main.py
    Co-authored-by: Vasyl Hnatiuk <104974881+vasylhnatiuk@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class BaseRobot:
     def __init__(self,
                  name: str,
                  weight: int,
-                 coords: list = None) -> None:
+                 coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
         if coords is None:
