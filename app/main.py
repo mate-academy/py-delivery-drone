3ac0c0ba881d0d5bb7299e78ce1ac4f02ce9f30c
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-29 17:49:56 +0300

    second_try

--- app/main.py
+++ app/main.py
@@ -12,16 +12,29 @@ class BaseRobot(Cargo):
         self.coords = coords
         self.coords = coords if coords else [0, 0]
 
-    def move(self, direction: str, step: int = 1) -> None:
-        directions = {
-            "forward": (0, step),
-            "back": (0, -step),
-            "right": (step, 0),
-            "left": (-step, 0)
-        }
-        if direction in directions:
-            self.coords[0] += directions[direction][0]
-            self.coords[1] += directions[direction][1]
+    def go_forward(self, step: int = 1) -> None:
+        self.coords[1] += step
+
+    def go_back(self, step: int = 1) -> None:
+        self.coords[1] -= step
+
+    def go_right(self, step: int = 1) -> None:
+        self.coords[0] += step
+
+    def go_left(self, step: int = 1) -> None:
+        self.coords[0] -= step
+
+    # def move(self, direction: str, step: int = 1) -> None:
+    #     directions = {
+    #         "forward": (0, step),
+    #         "back": (0, -step),
+    #         "right": (step, 0),
+    #         "left": (-step, 0)
+    #     }
+    #     if direction in directions:
+    #         self.coords[0] += directions[direction][0]
+    #         self.coords[1] += directions[direction][1]
+    # why isn't it working?
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -31,12 +44,11 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         super().__init__(name, weight, coords if coords else [0, 0, 0])
 
-    def move(self, direction: str, step: int = 1) -> None:
-        super().move(direction, step)
-        if direction == "up":
-            self.coords[2] += step
-        elif direction == "down":
-            self.coords[2] -= step
+    def go_up(self, step: int = 1) -> None:
+        self.coords[2] += step
+
+    def go_down(self, step: int = 1) -> None:
+        self.coords[2] -= step
 
 
 class DeliveryDrone(FlyingRobot):
@@ -55,8 +67,8 @@ class DeliveryDrone(FlyingRobot):
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
-    # намагався використати тернарний оператор для присвоєння,
-    # але виглядало жахливо
+    # tried to use the ternary operator to assign,
+    # but it looked terrible
 
     def unhook_load(self) -> None:
         self.current_load = None
