46bb818a0b45c37d703d3413f92b1861d1019664
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-07-30 09:43:03 +0300

    'code_optimization'

--- app/main.py
+++ app/main.py
@@ -40,8 +40,11 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: Optional[list] = None
     ) -> None:
-        coords = [0, 0, 0] if not coords else coords
-        super().__init__(name, weight, coords)
+        super().__init__(
+            name,
+            weight,
+            coords=[0, 0, 0] if not coords else coords
+        )
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
