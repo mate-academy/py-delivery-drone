2fe07a3435ca04f6f27090fa6db7dc78a18de799
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-07-30 09:10:49 +0300

    'code_improvements'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
+from typing import Optional
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: Optional[list] = None
+    ) -> None:
         self.name = name
         self.weight = weight
-        if not coords:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0] if not coords else coords
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -33,13 +38,10 @@ class FlyingRobot(BaseRobot):
             self,
             name: str,
             weight: int,
-            coords: list = None
+            coords: Optional[list] = None
     ) -> None:
         super().__init__(name, weight, coords)
-        if not coords:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        self.coords = [0, 0, 0] if not coords else coords
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -54,14 +56,10 @@ class DeliveryDrone(FlyingRobot):
             name: str,
             weight: int,
             max_load_weight: int,
-            current_load: Cargo = None,
-            coords: list = None
+            current_load: Optional[Cargo] = None,
+            coords: Optional[list] = None
     ) -> None:
         super().__init__(name, weight, coords)
-        if not coords:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
