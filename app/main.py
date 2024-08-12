3f4ec50d035c2ca5c055aca42b51d56f78a56ce0
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-12 11:46:52 +0300

    code refactor

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] | None = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords
@@ -28,7 +31,10 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] | None = None) -> None:
         super().__init__(name, weight)
         self.coords = coords
         if not self.coords:
@@ -46,8 +52,8 @@ class DeliveryDrone(FlyingRobot):
                  name: str,
                  weight: int,
                  max_load_weight: int,
-                 current_load: int,
-                 coords: list = None) -> None:
+                 current_load: int | None,
+                 coords: list[int] | None = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
