87d1c44f1419d7939d9ea46a1af9b7b1780407de
Serhii-Chubur <serhichubur15@gmail.com>
2024-08-12 16:41:05 +0300

    "if" replaced by "or"

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int,
+                 coords: list[int] = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = [0, 0] if coords is None else coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> list:
         self.coords[1] += step
@@ -30,9 +31,10 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self, name: str, weight: int,
+                 coords: list[int] = None) -> None:
         super().__init__(name, weight,
-                         coords=[0, 0, 0] if coords is None else coords)
+                         coords=coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> list:
         self.coords[2] += step
@@ -45,7 +47,7 @@ class FlyingRobot(BaseRobot):
 
 class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str, weight: int, max_load_weight: int,
-                 current_load: int, coords: list = None) -> None:
+                 current_load: int, coords: list[int] = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
