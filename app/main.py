d58233ad3bb1768536a10d0d8825321f1fd7fd59
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-13 09:35:33 +0300

    code refactor

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ class BaseRobot:
                  coords: list[int] | None = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords
-        if not self.coords:
-            self.coords = [0, 0]
+        self.coords = coords if coords is not None else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -36,9 +34,7 @@ class FlyingRobot(BaseRobot):
                  weight: int,
                  coords: list[int] | None = None) -> None:
         super().__init__(name, weight)
-        self.coords = coords
-        if not self.coords:
-            self.coords = [0, 0, 0]
+        self.coords = coords if coords is not None else [0, 0, 0]
 
     def go_up(self, z_change: int = 1) -> None:
         self.coords[2] += z_change
