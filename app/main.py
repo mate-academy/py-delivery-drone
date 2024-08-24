eceb6699d711dabf342363e8c2aa8f8218924cb2
Nastya Kalinina <kalininan.kalinina@gmail.com>
2024-08-24 11:54:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,27 +8,21 @@ class Cargo:
 
 class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            coords = [0, 0]
+        self.coords = coords or [0, 0]
         self.name = name
         self.weight = weight
-        self.coords = coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
-        print(self.coords)
 
     def go_back(self, step: int = 1) -> None:
         self.coords[1] -= step
-        print(self.coords)
 
     def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
-        print(self.coords)
 
     def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
-        print(self.coords)
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -37,18 +31,13 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
-        elif len(self.coords) == 2:
-            self.coords.append(0)
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
-        print(self.coords)
 
     def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
-        print(self.coords)
 
 
 class DeliveryDrone(FlyingRobot):
