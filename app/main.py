51de0c49dc9ab0926cf370db0fb5510c0e7ef18b
Ivan <vanya.bidnenko@gmail.com>
2024-08-28 12:29:56 +0300

    simplife 2.0

--- app/main.py
+++ app/main.py
@@ -12,10 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -42,8 +39,7 @@ class FlyingRobot(BaseRobot):
             weight: float,
             coords: list[int] = None
     ) -> None:
-        super().__init__(name, weight, coords)
-        self.coords = coords or [0, 0, 0]
+        super().__init__(name, weight, coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
