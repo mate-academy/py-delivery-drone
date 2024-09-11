544ab1533ba04a253dcd95f2804c83cec81c8dd7
Serhiy Dema <sergeydema@gmail.com>
2024-09-11 23:15:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,120 @@
+from __future__ import annotations
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+        self.name = name
+        self.weight = weight
+        if coords and len(coords) >= 2:
+            self.coords = coords
+        else:
+            self.coords = [0, 0]    # [x, y]
+
+    def go_forward(self, step: int = 1) -> None:
+        self.coords[1] += step  # [x, y + step]
+
+    def go_back(self, step: int = 1) -> None:
+        self.coords[1] -= step  # [x, y - step]
+
+    def go_right(self, step: int = 1) -> None:
+        self.coords[0] += step  # [x + step, y]
+
+    def go_left(self, step: int = 1) -> None:
+        self.coords[0] -= step  # [x - step, y]
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+        if not (coords and len(coords) == 3):
+            coords = [0, 0, 0]    # [x, y, z]
+        super().__init__(name, weight, coords)
+
+    def go_up(self, step: int = 1) -> None:
+        self.coords[2] += step  # [x, y, z + step]
+
+    def go_down(self, step: int = 1) -> None:
+        self.coords[2] -= step  # [x, y, z - step]
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name: str, weight: int, max_load_weight: int,
+                 coords: list = None, current_load: Cargo = None) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        if current_load and isinstance(current_load, Cargo):
+            self.current_load = current_load
+        else:
+            self.current_load = None
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if self.current_load is None and isinstance(cargo, Cargo) \
+                and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+
+    def unhook_load(self) -> None:
+        self.current_load = None
+
+# robot0 = BaseRobot(name="W", weight=3)
+# print(robot0.coords)
+#
+# robot1 = BaseRobot(name="W", weight=3, coords=[3])
+# print(robot1.coords)
+#
+# robot = BaseRobot(name="Walle", weight=34, coords=[3, -2])
+# robot.go_forward()
+# print(robot.coords)
+# # robot.coords == [3, -1]
+# robot.go_right(5)
+# print(robot.coords)
+# # robot.coords == [8, -1]
+
+
+# flying_robot = FlyingRobot(name="Mike", weight=11)
+# flying_robot.go_up(10)
+# print(flying_robot.coords)
+# # flying_robot.coords = [0, 0, 10]
+
+
+# cargo = Cargo(14)
+# drone = DeliveryDrone(
+#     name="Jim",
+#     weight=18,
+#     coords=[11, -4, 16],
+#     max_load_weight=20,
+#     current_load=None,
+# )
+# drone.hook_load(cargo)
+# # drone.current_load is cargo
+# print(
+#     cargo,
+#     drone.current_load
+# )
+#
+# cargo2 = Cargo(2)
+# drone.hook_load(cargo2)
+# print(
+#     cargo2,
+#     drone.current_load
+# )
+#
+# drone2 = DeliveryDrone(
+#     name="Jack",
+#     weight=9,
+#     max_load_weight=30,
+#     current_load=Cargo(20),
+# )
+# drone2.unhook_load()
+# # drone2.current_load is None
+#
+# print(
+#     drone2.__dict__,
+#     drone2.current_load
+# )
