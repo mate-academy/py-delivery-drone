93d78014aab68022c3c74c8c4aca1259c8f989ad
watergor <izhyvchyn@gmail.com>
2024-09-18 22:43:38 +0300

    Solution v0.1

--- app/main.py
+++ app/main.py
@@ -2,4 +2,29 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+        self.name = name
+        self.weight = weight
+        if coords:
+            self.coords = coords
+        else:
+            self.coords = [0, 0]    
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"    
+    
+    def go_forward(self,step: int = 1) -> None:
+        self.coords[1] += step
+    def go_back(self,step: int = 1) -> None:
+        self.coords[1] -= step
+    def go_right(self,step: int = 1) -> None:
+        self.coords[0] += step
+    def go_left(self,step: int = 1) -> None:
+        self.coords[0] -= step
+
+class FlyingRobot(BaseRobot):
+    pass
+
+class DeliveryDrone(FlyingRobot):
+    pass
