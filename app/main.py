7bd406643d65d5aade77ad88975f44c1804a74ef
watergor <izhyvchyn@gmail.com>
2024-09-19 22:18:05 +0300

    Solution with fixing

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+        self,
+        name: str,
+        weight: int,
+        coords: list[int] = None
+    ) -> None:
         self.name = name
         self.weight = weight
         if coords:
