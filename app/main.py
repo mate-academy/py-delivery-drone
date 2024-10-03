a8cdd20e0680fb632b6a5ef26c83cc02b5d7bf8b
Bohdan Poliienko <mignatov756@gmail.com>
2024-10-03 16:10:50 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -50,10 +50,10 @@ class FlyingRobot(BaseRobot):
         super().__init__(name, weight, coords)
         self.coords = coords if coords is not None else [0, 0, 0]
 
-    def go_up(self, step: int = 1):
+    def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
 
-    def go_down(self, step: int = 1):
+    def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
 
 
