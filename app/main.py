f6d78a2a6966d1cf9279d000be681dccd7be5542
roman <skyfoxwork@gmail.com>
2024-10-03 21:49:46 +0300

    self.coords = coords or [0, 0], [0, 0, 0]

--- app/main.py
+++ app/main.py
@@ -12,10 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -41,10 +38,7 @@ class FlyingRobot(BaseRobot):
             coords: list[int, int, int] = None
     ) -> None:
         super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
