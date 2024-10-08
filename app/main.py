af15d6209f105d7ca9f6bbe1d6c3bd7909c78dbd
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-08 16:54:45 +0300

    Fixed mentor's notes

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ class Cargo:
 
 class BaseRobot:
 
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list[int] = None) -> None:
+
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
