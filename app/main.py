798fcd81aef79cea177d22b64430a0995daf77c0
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-10-08 22:34:07 +0300

    Fixed values of the coords variable

--- app/main.py
+++ app/main.py
@@ -12,10 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if coords is None:
-            self.coords = [0, 0]
-        else:
-            self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -40,8 +37,7 @@ class FlyingRobot(BaseRobot):
             weight: int | float,
             coords: None | list[int] = None
     ) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
