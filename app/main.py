a3c9180fa9461514e90b22e8e2ccb8943c55e76c
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-10-09 12:08:05 +0300

    my_solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int,
+    def __init__(self,
+                 name: str,
+                 weight: int,
                  coords: list[int] = None) -> None:
         self.name = name
         self. weight = weight
@@ -27,9 +29,11 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list = None) -> None:
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
@@ -40,7 +44,8 @@ class FlyingRobot(BaseRobot):
 
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  weight: int,
                  max_load_weight: int,
                  current_load: Cargo = None,
@@ -50,7 +55,7 @@ class DeliveryDrone(FlyingRobot):
         self.current_load = current_load
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load is None and cargo.weight <= self.max_load_weight:
+        if not self.current_load and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
     def unhook_load(self) -> None:
